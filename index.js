98b48eb5076a42d94c730d0d6920872936576762
Sh Raj <66713844+SH20RAJ@users.noreply.github.com>
2023-07-19 18:48:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { TelegramBot } = require('telegram-bot-api');
 
 // Replace 'YOUR_TELEGRAM_BOT_TOKEN' with your actual bot token
 const token = '6190647327:AAFxS6-pwJldMaOtWmptqoFK9j5ABJd8KEs';
 
 // Create a new instance of the TelegramBot
-const bot = new TelegramBot(token, { polling: true });
+const bot = new TelegramBot(token);
 
-// Listen for the /hi command
-bot.onText(/\/hi/, (msg) => {
-  const chatId = msg.chat.id;
-  bot.sendMessage(chatId, 'Hi!');
+// Create an Express app
+const app = express();
+
+// Set up the middleware to parse incoming JSON requests
+app.use(bodyParser.json());
+
+// Handle incoming Telegram webhook requests
+app.post(`/webhook/${token}`, (req, res) => {
+  // Process the incoming update from Telegram
+  bot.processUpdate(req.body);
+
+  // Send a response to acknowledge receipt of the update
+  res.sendStatus(200);
+});
+
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running');
 });
